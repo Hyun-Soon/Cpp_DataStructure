52858539ec534f1e0e972289b4e60e58371233c5
Hyun-Soon <s7514036@naver.com>
2022-02-22 17:06:10 +0900

    02/22

--- Cpp_DataStructureAndAlgorithm/1_3_1.cpp
+++ Cpp_DataStructureAndAlgorithm/1_3_1.cpp
@@ -43,7 +43,32 @@ public:
 	{
 		if (index < n)
 			return data[index];
+		throw "Index out of range";
+	}
 
+	size_t size() const
+	{
+		return n;
+	}
+
+	~dynamic_array()
+	{
+		delete[] data; //prevent memory leak
+	}
+
+	T* begin() { return data; }
+	const T* begin() const { return data; }
+	T* end() { return data + n; }
+	const T* end() const { return data + n; }
+
+	friend dynamic_array<T> operator+(const dynamic_array<T>& arr1,
+		dynamic_array<T>& arr2)
+	{
+		dynamic_array<T> result(arr1.size() + arr2.size());
+		std::copy(arr1.begin(), arr1.end(), result.begin());
+		std::copy(arr2.begin(), arr2.end(), result.begin() + arr1.size());
+
+		return result;
 	}
 };
 
